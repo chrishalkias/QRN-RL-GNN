a478dc2f921b79e8d7eaf4dc77580810334acb90
chrishalkias <chrishalkias@pm.me>
2025-03-16 01:01:24 +0100

    main file change

--- main.py
+++ main.py
@@ -2,12 +2,33 @@
 
 """
 Created on Thu 6 Mar 2025
+The main simulation file. 
+Dependencies: experiment.py, quantum_network_env.py, graph_models.py, repeater_network.py
+Author: Chris Chalkias
 """
 
 import experiment as exp
 import numpy as np
 import matplotlib.pyplot as plt
 
+N = 7
+TAU = 10000
+P_ENTANGLE = 1
+P_SWAP = 1
+MODEL = "DQN"
+FILE_NAME = None
+TRAINING_PLOTS = None
+TRAINNING_PARAMETERS = None
+MODEL_FILES = None
+
 if __name__ == "__main__":
-    experiment = exp(model = "DQN", n=7, tau=10000, p_entangle=1, p_swap=1)
+    experiment = exp(model = "DQN",
+                    n=N, tau=TAU,
+                    p_entangle=P_ENTANGLE,
+                    p_swap=P_SWAP,
+                    #file_name=FILE_NAME,
+                    #training_plots=TRAINING_PLOTS,
+                    #training_parameters=TRAINNING_PARAMETERS,
+                    #model_files=MODEL_FILES,
+                            )
     experiment.display_info()
