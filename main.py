176d70360c607887a21d3cfb9b6f83969bac6055
chrishalkias <chrishalkias@pm.me>
2025-04-24 09:32:05 +0200

    created subfolders

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ Part of: MSc Thesis titled "Quantum Network Reinforcement Learning"
 """
 
 import numpy as np
+import version_information as vinf
 np.set_printoptions(legacy='1.25')
 import matplotlib.pyplot as plt
 
@@ -35,6 +36,9 @@ TRAINING_PLOTS = None
 TRAINNING_PARAMETERS = None
 MODEL_FILES = None
 
+with open("version_information.txt", "w") as file:
+    print()
+
 if __name__ == "__main__":
 
     # Run DQN with gym
@@ -42,22 +46,29 @@ if __name__ == "__main__":
         experiment = exp(n=N_train, tau=TAU,
                     p_entangle=P_ENTANGLE,
                     p_swap=P_SWAP,
-                    log_dir = TRAIN_LOG_DIR,
-                            )
+                    log_dir = TRAIN_LOG_DIR)
+        
         experiment.display_info()
+        
         if TRAIN_AGENT:
             experiment.train_agent(total_timesteps=TRAIN_STEPS, plot=PLOT_TRAINING_METRICS, callback=False)
+
         if EVALUATE_AGENT:
             experiment.test_agent(max_steps =10, render=RENDER_EVALUATION)
-        if True:
-            experiment.env.close()
-            print("Program exited with exit code 0")
+        
+        experiment.env.close()
+        print("Program exited with exit code 0")
 
 
     # run DQN with CNN network and torch        
     elif MODEL == "CNN":
-        exp = agent(n=N_train, kappa=1, tau=1_000_000,p_entangle=.85, p_swap=.85, lr=0.001, gamma=0.9, epsilon=0.1)
+
+        exp = agent(n=N_train, kappa=1, tau=1_000_000,
+                    p_entangle=P_ENTANGLE, p_swap=P_SWAP,
+                    lr=0.001, gamma=0.9, epsilon=0.1)
+        
         if TRAIN_AGENT:
-            exp.train(episodes=1_000, plot=True)
+            exp.train(episodes=TRAIN_STEPS, plot=True)
+
         if EVALUATE_AGENT:
-            agent.test(N_test, max_steps=100, kind='trained', plot=True)
+            agent.test(N_test, max_steps=100, kind='trained', plot=RENDER_EVALUATION)
