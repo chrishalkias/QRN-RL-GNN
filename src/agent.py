ff6af96322a2849a133b66d00283b530bab35ec3
chrishalkias <chrishalkias@pm.me>
2025-11-04 14:21:43 +0100

    docs: extra function documentation

--- src/agent.py
+++ src/agent.py
@@ -24,7 +24,26 @@ class AgentGNN(RepeaterNetwork):
                gamma=0.95,
                epsilon=1):
     
-    """This class implements the **RL agent** used to learn strategies on the **Repeater Network**"""
+    """
+    This class implements the RL agent used to learn strategies on the Repeater Network.
+    It also includes the heuristics tested against.
+
+    Composed from: RepeaterNetwork
+
+    Methods:
+      get_state_vector()
+      get_valid_actions()
+      choose_action()
+      update_environment()
+      reward()
+      train()
+      saveModel()
+      trained_action()
+      random_action()
+      alternating_action()
+      swap_asap()
+      test()
+    """
 
     super().__init__(n, directed, geometry, kappa, tau, p_entangle, p_swap)
     self.lr=lr
@@ -42,12 +61,20 @@ class AgentGNN(RepeaterNetwork):
     self.target_model.eval()
     self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
 
-  def get_state_vector(self):
-    """Returns the state of entanglements in the network in the form of a pyG.Data"""
+  def get_state_vector(self) -> torch.tensor:
+    """Returns the state of entanglements in the network (pyG.Data)"""
     return self.tensorState() #Data structure with x=torch.ones and edge_attr
 
-  def get_valid_actions(self):
-    """Return indices of valid actions in flattened q_values"""
+  def get_valid_actions(self) -> list:
+    """
+    If the repeater is the last of the chain it dissalows EG to the right.
+    Checks if there is a left and a right connection for each repeater.
+    If both exists, it allows swap on that repeater.
+    Appends an even number (node*2) for EG and an odd one (node*2 +1) for swaps
+
+    Returns:
+      valid_actions (list) : 
+    """
     valid_actions = []
     for node in range(self.n):
         if node < self.n - 1:  # Can entangle right
@@ -64,7 +91,8 @@ class AgentGNN(RepeaterNetwork):
               valid_actions.append(node * 2 + 1)
     return valid_actions
 
-  def choose_action(self, use_trained_model=False):
+  def choose_action(self, use_trained_model=False) -> int:
+    """Epsilon-greedy action choice"""
     if not use_trained_model and random.uniform(0, 1) < self.epsilon:
         valid_actions = self.get_valid_actions()
         return random.choice(valid_actions)
@@ -84,18 +112,30 @@ class AgentGNN(RepeaterNetwork):
         # Select action with highest probability (equivalent to argmax but safer)
         return torch.multinomial(action_probs, 1).item()
 
-  def update_environment(self, action):
+  def update_environment(self, action) -> float:
+    """Applies the action to the environment and returns the reward.
+    The action is of the form `self.entangle((i,j))` or `self.swapAT(k)`.
+    See the `RepeaterNetwork` documentation for refference.
+    """
     exec(self.new_actions()[action])
     return self.reward()
 
-  def reward(self):
-    """Computes the agents reward"""
+  def reward(self) -> float:
+    """Computes the agents reward.
+    This is based off the end-to-end condition(+1/-0.01)
+    And a bonus reward that is proportional to the existing links age and length.
+    """
     bonus_reward = 0 # some function f(d, e; n)
     for (i,j), (adj, entanglement) in self.matrix.items():
         bonus_reward +=  entanglement*(j-i)/(self.n**2) if entanglement else 0
     return 1 if self.endToEndCheck() else -0.01 + bonus_reward/10
 
-  def train(self, episodes=1000, plot=True, save_model=True, savefig=True):
+  def train(self, 
+            episodes=1000, 
+            plot=True, 
+            save_model=True, 
+            savefig=True) -> list:
+    
     """Trains the agent"""
     self.reset()
 
@@ -172,25 +212,26 @@ class AgentGNN(RepeaterNetwork):
     torch.save(self.model.state_dict(), dir)
     print(f"Model saved to {dir}")
 
-  def trained_action(self):
+  def trained_action(self) -> int:
+    """Returns the action with the highest Q-value"""
     return self.choose_action(use_trained_model=True)
 
-  def random_action(self):
+  def random_action(self) -> int:
       """Perform a random action at each node"""
       waits = ['' for _ in range(self.n)]
       entangles = [f'self.entangle({(i,i+1)})' for i in range(self.n-1)]
       swaps = [f'self.swapAT({i})' if (i != 0) and (i !=self.n-1) else '' for i in range(self.n)] # dont swap ad end nodes
       return [random.choice([e, s, w]) for e, s, w in zip(entangles, swaps, waits) if random.choice([e, s, w]) is not None]
 
-  def alternating_action(self, step):
+  def alternating_action(self, step) -> int:
       """At even timestep entangle all and at odd swap all"""
       if (step % 2) == 0:
           return [f'self.entangle({(i,i+1)})' for i in range(self.n-1)]
       elif (step % 2) == 1:
           return [f'self.swapAT({i})' if (i != 0) and (i !=self.n-1) else '' for i in range(self.n)]
 
-  def swap_asap(self):
-      """Perform the swap asap"""
+  def swap_asap(self) -> int:
+      """Performs the swap asap"""
       net = self
       actions = []
 
@@ -206,7 +247,16 @@ class AgentGNN(RepeaterNetwork):
               actions.append(f'self.entangle(edge={(i,i+1)})')
       return actions
 
-  def test(self, n_test=5, p_entangle=1, p_swap=1, tau=1000, max_steps=100, verbose=False, kind='trained'):
+  def test(self, 
+           n_test=5, 
+           p_entangle=1, 
+           p_swap=1, 
+           tau=1000, 
+           max_steps=100, 
+           verbose=False, 
+           kind='trained') -> list:
+    
+    """Perform the validation of the agent against the heuristic strategies"""
     super().__init__(n=n_test, tau=tau, p_entangle=p_entangle, p_swap=p_swap)
     totalReward, rewardList = 0, []
     fidelity, fidelityList = 0,[]
@@ -250,8 +300,8 @@ class AgentGNN(RepeaterNetwork):
           exec(action)
           reward += self.reward()
           links_established +=1 if self.endToEndCheck() else 0
-
           self.reset() if self.endToEndCheck() else None
+
       print(f"Round: {step}, Reward: {reward:.3f}") if verbose else None
       totalReward += reward
       rewardList.append(totalReward)
