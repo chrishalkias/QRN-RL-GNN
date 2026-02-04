0c167b56d7d95756593250b80547b4f568e0b98b
chrishalkias <chrishalkias@pm.me>
2026-02-04 16:45:26 +0100

    code: minor changes

--- src/buffer.py
+++ src/buffer.py
@@ -2,14 +2,13 @@ from torchrl.data import ReplayBuffer
 from torchrl.data import LazyMemmapStorage, LazyTensorStorage, ListStorage
 import numpy as np
 from repeaters import RepeaterNetwork
-from agent import AgentGNN
 import random
 from tensordict import TensorDict
 import torch
 
 
 
-class ExperienceBuffer():
+class Buffer():
     def __init__(self, max_size):
         """
         Implements a simple list replay buffer for the agent
@@ -19,12 +18,10 @@ class ExperienceBuffer():
             add()
             sample()
         """
-        self.max_size = max_size
+        self.max_size = max_size # 5e4-1e6
         self.buffer_list = ReplayBuffer(storage=ListStorage(max_size), 
                            collate_fn=lambda x: x)
         
-    def size(self) -> int:
-        return len(self.buffer_list)
 
     def add(self, state, action, reward, next_state) -> None:
         data = TensorDict({
@@ -37,13 +34,15 @@ class ExperienceBuffer():
     def sample(self, samples = 1) -> list:
         return self.buffer_list.sample(samples)
 
+    def size(self) -> int:
+        return len(self.buffer_list)
 
 
 
 
-if __name__ == '__main__':
+if False:
     agent = AgentGNN(n=4)
-    buffer = ExperienceBuffer(max_size = 10_000)
+    buffer = Buffer(max_size = 10_000)
 
     # collect data
     state = agent.get_state_vector()
@@ -58,17 +57,10 @@ if __name__ == '__main__':
 
     x = buffer.sample(3)
 
-    # print the (S,A,R) tuple
+    #print the (S,A,R) tuple
+
     print(x[0]['s'])
     print(x[0]['a'])
     print(x[0]['r'])
     print(x[0]['s_'])
 
-    # pseudo algorithm implementation
-    step = int()
-    k = int()
-    s,a,r,s_ = None, None, None, None
-    if step < buffer.max_size:
-        buffer.add(s,a,r,s_)
-    else:
-        buffer.sample(samples=k)
